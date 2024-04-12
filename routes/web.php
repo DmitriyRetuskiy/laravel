c3d78b1701fb5d5e71769ab32befe8578bede9ef
Dmitriy <reutskiy-dmitriy@mail.ru>
2024-04-12 16:02:35 +0300

    [fix] step to loop

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use App\Http\Middleware;
+
+$midlleware = new  Middleware\Person;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [Controllers\UserController::class,'index']);
+
+
+Route::get('/falcon', [Controllers\UserController::class,'falconTemplate'])->name('template');
+
+Route::get('/redis', [Controllers\UserController::class,'redisShow'])->name('redis.show')->middleware('person');
+
+Route::group(['prefix' => 'mail'], function (){
+    Route::get('/mail', [Controllers\UserController::class, 'mailForm'])->name('mail.form');
+    Route::post('/mail', [Controllers\UserController::class, 'mail'])->name('mail');
+});
+
+Route::get('/first-or-create', [Controllers\UserController::class,'firstOrCreate'])->name('firstOrCreate');
+
+
+Route::resource('post',Controllers\PostController::class);
