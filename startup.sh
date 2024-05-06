4829572e85c5f5f0f6dfe0fddeba0faf725fc42f
Dim <reutskiy-dmitriy@mail.ru>
2024-05-06 17:38:48 +0300

    [fix] new time %time%

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo "-----------------------";
 echo "----Git update repo----";
 echo "-----------------------";
-sleep 5 &
+sleep 5 
 echo "This is updated from date %date% time %time%">"startup.txt"
 git add .
 git commit -m "[fix] new time %time%"
