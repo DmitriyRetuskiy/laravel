5a5ebbca49700339099f8776eaa8981152c8d4e6
Dim <reutskiy-dmitriy@mail.ru>
2024-05-13 17:30:10 +0300

    [fix] new time Mon May 13 05:30:10 PM MSK 2024

--- startup.sh
+++ startup.sh
@@ -5,6 +5,6 @@ sleep 5
 start_time=$(date) 
 echo "This is updated from date $start_time">"startup.txt"
 git add .
-git commit -m "[fix] new time %time%"
+git commit -m "[fix] new time $start_time"
 git push origin master
 
