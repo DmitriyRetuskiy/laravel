14761a05651868a9566ddb6e1b364bf39fd52077
Dim <reutskiy-dmitriy@mail.ru>
2024-05-17 12:42:09 +0300

    [fix] new time Fri May 17 12:42:09 PM MSK 2024

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 echo "-----------------------";
 echo "----Git update repo----";
 echo "-----------------------";
 sleep 5
 start_time=$(date) 
 echo "This is updated from date $start_time">"startup.txt"
-git add .
-git commit -m "[fix] new time $start_time"
-git push origin master
+git -C "/home/dim/Public/domains/laravel" add .
+git -C "/home/dim/Public/domains/laravel" commit -m "[fix] new time $start_time"
+git -C "/home/dim/Public/domains/laravel" push origin master
 
