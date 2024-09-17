31ef45a472c188e2a8cfee5bfc35af509053eb31
Dim <reutskiy-dmitriy@mail.ru>
2024-09-17 11:26:36 +0300

    [fix] new time Tue Sep 17 11:26:36 AM MSK 2024

--- startup.sh
+++ startup.sh
@@ -5,8 +5,13 @@ echo "-----------------------";
 sleep 3
 start_time=$(date) 
 echo "This is updated from date $start_time">"/home/dim/Public/domains/laravel/startup.txt"
-git -C "/home/dim/Public/domains/laravel" add .
-git -C "/home/dim/Public/domains/laravel" commit -m "[fix] new time $start_time"
-git -C "/home/dim/Public/domains/laravel" push origin master
-$SHELL
+git add .
+git commit -m "[fix] new time $start_time"
+git push origin master
+echo "Updated success"
+# ---- for not current folder in git we ca use ----
+#git -C "/home/dim/Public/domains/laravel" add .
+#git -C "/home/dim/Public/domains/laravel" commit -m "[fix] new time $start_time"
+#git -C "/home/dim/Public/domains/laravel" push origin master
+#$SHELL
 
